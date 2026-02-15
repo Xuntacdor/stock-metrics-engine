a75781fbc5c66a4f73a563f4ffffd8246a7d0bad
Nguyen Nhat Quang <nguyennhatquang@pop-os.lan>
2026-02-15 21:35:08 +0700

    feat(config): integrate DotNetEnv to load variables from .env

---
+++ src/backend-api.Api/Program.cs
@@ -0,0 +1,34 @@
+using backend_api.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
+
+Env.Load();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<QuantIQContext>(options =>
+    options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
